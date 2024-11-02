288b5fbeec342b6d227f08d34332de472da284db
Dowon <ks2515@naver.com>
2024-11-02 20:41:56 +0900

    style: apply formatter

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 import pickle
-from uuid import uuid4
-from tempfile import TemporaryDirectory
 from pathlib import Path
+from tempfile import TemporaryDirectory
+from uuid import uuid4
 
 import pytest
 from hypothesis import given, settings
